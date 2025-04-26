9988e10ed523cb004a0a593cb17165b77dd811a1
Ebrahim Alshaker <alshaker@proton.me>
2025-04-26 09:07:21 +0300

    Standarize FastAPI main documentation

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from config.config import Settings
 
 
-app=FastAPI()
+app=FastAPI(title="Family Buget Tracker RESTFul APIs",
+            summary="Collection for APIs to be act as a middleware between Mobile/Web front-ends and database and include most of the business logic",
+            description="""The Multi-Family Budget Tracking Application is a simple secure and collaborative financial management tool that allows multiple families to independently track their budgets, expenses, goals, and transactions. With built-in support for multi-tenancy, each family’s data is isolated and confidential.
+Users can create accounts, categorize transactions, assign budgets, and track savings goals. This RestFull API system features a flexible many-to-many relationship between families and users, enabling shared access across households with role-based permissions.
+Designed with a RESTful API structure, the application supports integration with web and mobile interfaces, empowering families to plan and manage their finances together — with clarity, control, and transparency.""",
+            version="0.0.0",
+            docs_url="https://github.com/bhalshaker/saas-family-budget-tracker")
 #app.include_router(UsersRouter, prefix="/api") 
 settings=Settings()
