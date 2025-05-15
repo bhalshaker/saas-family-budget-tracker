aa08e1ad5235fc959b0ea4201f01283b7d48c267
Ebrahim Alshaker <alshaker@proton.me>
2025-05-15 17:46:12 +0300

    Add budget transaction controller layout

---
+++ controllers/budget_transaction.py
@@ -0,0 +1,17 @@
+from models import UserModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+async def get_all_budget_transactions_of_family(family_id: str, current_user: UserModel, db: AsyncSession):
+    pass
+
+async def add_budget_transaction_for_family(family_id: str, new_budget_transaction: dict, current_user: UserModel, db: AsyncSession):
+    pass
+
+async def retrieve_budget_transaction(budget_transaction_id: str, current_user: UserModel, db: AsyncSession):
+    pass
+
+async def update_budget_transaction(budget_transaction_id: str, updated_budget_transaction: dict, current_user: UserModel, db: AsyncSession):
+    pass
+
+async def delete_budget_transaction(budget_transaction_id: str, current_user: UserModel, db: AsyncSession):
+    pass
