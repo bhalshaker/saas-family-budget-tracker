e0e6273092dee021fc9fbb36dfca0a36d59279d4
Ebrahim Alshaker <alshaker@proton.me>
2025-05-15 21:28:42 +0300

    Update serializers in controllers

--- controllers/budget_transaction.py
+++ controllers/budget_transaction.py
@@ -1,14 +1,15 @@
 from models import UserModel
 from sqlalchemy.ext.asyncio import AsyncSession
+from serializers import CreateCategory, UpdateCategory, RestCreateCategoryResponse, RestGetCategoryResponse, RestGetAllCategoriesOfamilyResponse, BaseRestResponse
 
-async def get_all_budget_transactions_of_family(family_id: str, current_user: UserModel, db: AsyncSession):
+async def get_all_budget_transactions_of_family(family_id: str, current_user: UserModel, db: AsyncSession)->RestGetAllBudgetTransactionsOfamilyResponse:
     pass
 
-async def add_budget_transaction_for_family(family_id: str, new_budget_transaction: dict, current_user: UserModel, db: AsyncSession):
+async def add_budget_transaction_for_family(family_id: str, new_budget_transaction: dict, current_user: UserModel, db: AsyncSession)-> RestCreateBudgetTransactionResponse:
     pass
 
-async def retrieve_budget_transaction(budget_transaction_id: str, current_user: UserModel, db: AsyncSession):
+async def retrieve_budget_transaction(budget_transaction_id: str, current_user: UserModel, db: AsyncSession)->RestGetBudgetTransactionResponse:
     pass
 
-async def delete_budget_transaction(budget_transaction_id: str, current_user: UserModel, db: AsyncSession):
+async def delete_budget_transaction(budget_transaction_id: str, current_user: UserModel, db: AsyncSession)->BaseRestResponse:
     pass
--- controllers/transaction.py
+++ controllers/transaction.py
@@ -1,18 +1,18 @@
 from models import UserModel
 from sqlalchemy.ext.asyncio import AsyncSession
-from serializers import CreateTransaction, UpdateTransaction
+from serializers import CreateTransaction, UpdateTransaction, RestCreateTransactionResponse, RestGetTransactionResponse, RestGetAllTransactionsOfamilyResponse, BaseRestResponse
 
-async def get_all_transactions_of_family(family_id: str, current_user: UserModel, db: AsyncSession):
+async def get_all_transactions_of_family(family_id: str, current_user: UserModel, db: AsyncSession)->RestGetAllTransactionsOfamilyResponse:
     pass
 
-async def create_transaction_for_family(family_id: str, new_transaction: CreateTransaction, current_user: UserModel, db: AsyncSession):
+async def create_transaction_for_family(family_id: str, new_transaction: CreateTransaction, current_user: UserModel, db: AsyncSession)-> RestCreateTransactionResponse:
     pass
 
-async def retrieve_transaction(transaction_id: str, current_user: UserModel, db: AsyncSession):
+async def retrieve_transaction(transaction_id: str, current_user: UserModel, db: AsyncSession)-> RestGetTransactionResponse:
     pass
 
-async def update_transaction(transaction_id: str, updated_transaction: UpdateTransaction, current_user: UserModel, db: AsyncSession):
+async def update_transaction(transaction_id: str, updated_transaction: UpdateTransaction, current_user: UserModel, db: AsyncSession)->RestCreateTransactionResponse:
     pass
 
-async def delete_transaction(transaction_id: str, current_user: UserModel, db: AsyncSession):
+async def delete_transaction(transaction_id: str, current_user: UserModel, db: AsyncSession)->BaseRestResponse:
     pass
