a828fc8fdb5900bb5d84cf618c821c6299797b3f
Ebrahim Alshaker <alshaker@proton.me>
2025-05-16 13:29:35 +0300

    Update families router class

--- routes/families.py
+++ routes/families.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from serializers import RestFamilyCreationResponse,RestGetAllFamiliesResponse,CreateFamily
-from controllers import ControllerCreateFamily,ControllerGetFamily,ControlerGetAllFamilies,ControllerDeleteFamily,ControllerUpdateFamily
+from controllers import ControllerCreateFamily,ControllerGetFamily,ControllerGetAllFamilies,ControllerDeleteFamily,ControllerUpdateFamily
 from models import UserModel
 from controllers import get_current_user
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,7 +14,7 @@ router = APIRouter()
             description="""Get all families in the system.
             This endpoint allows you to retrieve a list of all families in the system. The families will be returned in the response.""")
 async def get_all_families(db: AsyncSession = Depends(get_db))->RestGetAllFamiliesResponse:
-    pass
+    return await ControllerGetAllFamilies(db)
 
 @router.post(path="/api/v1/families/",
              response_model=RestFamilyCreationResponse,summary="Create a new family",
@@ -38,17 +38,47 @@ async def create_family(new_family: CreateFamily,current_user: UserModel = Depen
             description="""Get family by ID.
             This endpoint allows you to retrieve a family by its ID. The family details will be returned in the response.""")
 async def get_family(family_id: str,current_user:UserModel=Depends(get_current_user),db: AsyncSession = Depends(get_db))->RestFamilyCreationResponse:
-    pass
+    """
+    Retrieve information about a specific family.
+    Args:
+        family_id (str): The unique identifier of the family to retrieve.
+        current_user (UserModel, optional): The currently authenticated user, injected by dependency.
+        db (AsyncSession, optional): The asynchronous database session, injected by dependency.
+    Returns:
+        RestFamilyCreationResponse: The response object containing the family's details.
+    """
+
+    return await ControllerGetFamily(family_id=family_id,current_user=current_user,db=db)
 
 @router.delete(path="/api/v1/families/{family_id}",
             response_model=RestFamilyCreationResponse,summary="Delete family by ID",
             description="""Delete family by ID.
             This endpoint allows you to delete a family by its ID. The family will be removed from the system.""")
 async def delete_family(family_id: str,current_user:UserModel=Depends(get_current_user),db: AsyncSession = Depends(get_db))->RestFamilyCreationResponse:
-    pass
+    """
+    Deletes a family by its ID.
+    Args:
+        family_id (str): The unique identifier of the family to delete.
+        current_user (UserModel, optional): The currently authenticated user, injected by dependency.
+        db (AsyncSession, optional): The asynchronous database session, injected by dependency.
+    Returns:
+        RestFamilyCreationResponse: The response object indicating the result of the deletion operation.
+    """
+
+    return await ControllerDeleteFamily(family_id=family_id,current_user=current_user,db=db)
 @router.put(path="/api/v1/families/{family_id}",
             response_model=RestFamilyCreationResponse,summary="Update family by ID",
             description="""Update family by ID.
             This endpoint allows you to update a family by its ID. The updated family details will be returned in the response.""")
 async def update_family_by_id(family_id: str,current_user:UserModel=Depends(get_current_user),db: AsyncSession = Depends(get_db))->RestFamilyCreationResponse:
-    pass
+    """
+    Update a family record by its ID.
+    Args:
+        family_id (str): The unique identifier of the family to update.
+        current_user (UserModel, optional): The currently authenticated user, injected by dependency.
+        db (AsyncSession, optional): The asynchronous database session, injected by dependency.
+    Returns:
+        RestFamilyCreationResponse: The response containing the updated family information.
+    """
+
+    return await ControllerUpdateFamily(family_id=family_id,current_user=current_user,db=db)
