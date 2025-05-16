8076795ab31c7e6cd5fe2ec394ed685558c3c50d
Ebrahim Alshaker <alshaker@proton.me>
2025-05-16 20:33:43 +0300

    Update budget controller,router,serializer classes

--- serializers/budget.py
+++ serializers/budget.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
+from typing import Optional,List
+from uuid import UUID
+from serializers import BaseRestResponse
 
 class CreateBudget(BaseModel):
     category_id: str
@@ -15,3 +17,21 @@ class UpdateBudget(BaseModel):
     amount: Optional[float] = None
     start_date: Optional[datetime] = None
     end_date: Optional[datetime] = None
+
+class BudgetInfo(BaseModel):
+    id:UUID
+    family_id:UUID
+    user_id:UUID
+    category_id: UUID
+    amount: float
+    start_date: datetime
+    end_date: datetime
+
+class RestCreateBudgetResponse(BaseRestResponse):
+    budget: BudgetInfo
+
+class RestGetBudgetResponse(BaseRestResponse):
+    budget: BudgetInfo
+
+class RestGetAllBudgetsOfamilyResponse(BaseRestResponse):
+    budgets: Optional[List[BudgetInfo]]=None
